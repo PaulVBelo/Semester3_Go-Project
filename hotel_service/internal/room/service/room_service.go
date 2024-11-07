ad9883b0f5093f9efed0459b661bbb8b5506672d
PaulVBelo <144692494+PaulVBelo@users.noreply.github.com>
2024-11-07 21:06:27 +0300

    More dto changes

--- hotel_service/internal/room/service/room_service.go
+++ hotel_service/internal/room/service/room_service.go
@@ -3,7 +3,7 @@ package service
 import "hotel_service/internal/server/dto"
 
 type RoomService interface {
-	GetByID(id int64) (dto.RoomDTO, error)
-	CreateRoom(toCreate *dto.RoomDTO) error
-	UpdateRoom(toUpdate *dto.RoomDTO) error
+	GetByID(id int64) (dto.RoomResponseDTO, error)
+	CreateRoom(toCreate *dto.RoomRequestDTO, hotel_id int64) (error, dto.RoomResponseDTO)
+	UpdateRoom(toUpdate *dto.RoomRequestDTO) (error, dto.RoomResponseDTO)
 }
