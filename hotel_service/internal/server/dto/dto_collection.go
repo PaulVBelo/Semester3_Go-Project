e25e5d3cf680a4fed120304056426a8c69395248
PaulVBelo <144692494+PaulVBelo@users.noreply.github.com>
2024-11-07 20:35:16 +0300

    small dto updates

--- hotel_service/internal/server/dto/dto_collection.go
+++ hotel_service/internal/server/dto/dto_collection.go
@@ -1,22 +1,33 @@
 package dto
 
-type RoomDTO struct {
+type RoomRequestDTO struct {
+	Name		string
+	Price		string
+	Rooms		[]RoomRequestDTO
+}
+
+type HotelRequestDTO struct {
+	Name		string
+	Adress		string
+}
+
+type RoomResponseDTO struct {
 	ID 			int64
 	Name		string
 	HotelID 	int64
 	Price		string
-	Amenities	[]AmenityDTO	`json:"omitempty"`
+	Amenities	[]AmenityResponseDTO	`json:"omitempty"`
 }
 
-type AmenityDTO struct {
+type AmenityResponseDTO struct {
 	ID 			int64
 	Name		string
 }
 
-type HotelDTO struct {
+type HotelResponseDTO struct {
 	ID			int64
 	Name		string
 	Adress		string
-	Rooms		[]RoomDTO
-	Amenities	[]AmenityDTO
+	Rooms		[]RoomResponseDTO
+	Amenities	[]AmenityResponseDTO
 }
